c2671c8adc797ee8c4717072d283aa7c20c86cca
Igor Bodrov <bodrovigor02@gmail.com>
2023-04-18 17:58:43 +0300

    feat: 🎸 Главная: разметка, стилизация, анимации

--- src/js/index.js
+++ src/js/index.js
@@ -2,52 +2,17 @@ import { lock, unlock } from 'tua-body-scroll-lock'
 
 // --- Мобильное меню
 let burger = document.getElementById('burger');
-let mobile_menu = document.getElementById('mobile_menu');
+let menu_graphic = document.querySelector('.header__nav-graphic');
+console.log(menu_graphic);
 
 // - Открытие по по клику на бургер
 burger.addEventListener('click', () => {
   burger.classList.toggle('main-nav__burger--active');
-  mobile_menu.classList.toggle('main-nav__nav-menu--mobile--active');
+  menu_graphic.classList.toggle('header__nav-graphic--active');
+  console.log(menu_graphic);
 
-  burger.classList.contains('main-nav__burger--active') ? lock(mobile_menu) : unlock(mobile_menu);
 
-})
-
-// --- Анимация печатающегося текста на главной странице
-window.addEventListener('load', () => {
-  if (document.getElementById('index_typing_text') === null) {
-    return;
-  }
-  else {
-    let i = 0;
-    let txt = document.querySelector('#index_typing_text').dataset.typing;
-    let speed = 100;
-
-    function typeWriter() {
-      if (i < txt.length) {
-        document.getElementById('index_typing_text').innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-      }
-    }
-
-    setTimeout(() => {
-      typeWriter();
-    }, 6000)
-
-  }
-})
-
-// --- Добавление интерактивного класса для всех ссылок на странице
-window.addEventListener('load', () => {
-  if (document.querySelector('a') === null) {
-    return;
-  }
-  else {
-    document.querySelectorAll('a').forEach((link) => {
-      link.classList.add('follow-change');
-    })
-  }
+  // burger.classList.contains('main-nav__burger--active') ? lock(mobile_menu) : unlock(mobile_menu);
 })
 
 // --- Бегущая строка
--- src/js/libs/gsap.js
+++ src/js/libs/gsap.js
@@ -1,10 +1,13 @@
 import { gsap } from "gsap";
+import { lock, unlock } from 'tua-body-scroll-lock'
+
+// --- Анимация меню на главной странице
 
 
 // --- Следяющий за курсором элемент
-let circle = document.getElementById('circle_follow');
-function moveCircle(e) {
-  gsap.to(circle, { css: { left: e.clientX, top: e.clientY }, duration: 0.1, });
-}
+// let circle = document.getElementById('circle_follow');
+// function moveCircle(e) {
+//   gsap.to(circle, { css: { left: e.clientX, top: e.clientY }, duration: 0.1, });
+// }
 
-window.addEventListener('mousemove', moveCircle);
+// window.addEventListener('mousemove', moveCircle);
