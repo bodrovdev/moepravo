879936c423c85bea1d7e8c6dad0b875326c99e0e
Igor Bodrov <bodrovigor02@gmail.com>
2023-05-11 15:40:09 +0300

    feat: 🎸 Адаптив, исправления

--- src/js/libs/gsap.js
+++ src/js/libs/gsap.js
@@ -1,58 +1,66 @@
 import { gsap } from "gsap";
-import { lock, unlock } from 'tua-body-scroll-lock'
 
+// --- Анимация крутящейся ссылки в блоке features на главной странице
 window.addEventListener('load', () => {
   if (document.querySelector('.features') === null) {
     return;
   }
   else {
-    let features_items = document.querySelectorAll('.features__item');
+    let features_items = document.querySelectorAll('.features__slider-slide');
 
     features_items.forEach((item) => {
 
       let features_tl = gsap.timeline({ repeat: -1, paused: true });
       features_tl.add(gsap.to(item.querySelector('.round-link__text div'), { rotate: 360, duration: 6, ease: "none" }))
 
-      item.addEventListener('mouseover', () => {
-        features_tl.play();
-      })
+      if (window.innerWidth >= 1280) {
+        item.addEventListener('mouseover', () => {
+          features_tl.play();
+        })
 
-      item.addEventListener('mouseleave', () => {
-        features_tl.pause();
-      })
+        item.addEventListener('mouseleave', () => {
+          features_tl.pause();
+        })
+      }
+      else {
+        setTimeout(() => {
+          features_tl.play();
+        }, 500)
+      }
     })
   }
 })
 
+// --- Анимации в блоке features на странице услуги
 window.addEventListener('load', () => {
   if (document.querySelector('.single-services__features') === null) {
     return;
   }
   else {
-    // 3
+    // - Второй элемент
     let img_3_1 = document.getElementById('serv-img-3-1');
     let img_3_2 = document.getElementById('serv-img-3-2');
     let img_3_3 = document.getElementById('serv-img-3-3');
 
-    // 3-1
+    // - Второй элемент - 1
     let img_3_1_tl = gsap.timeline({ repeat: -1 });
     img_3_1_tl.add(gsap.to(img_3_1, { x: 280, duration: 10, ease: "linear", }));
     img_3_1_tl.add(gsap.to(img_3_1, { zIndex: 4, duration: 0, }));
     img_3_1_tl.add(gsap.to(img_3_1, { x: 0, duration: 10, ease: "linear" }));
 
-    // 3-2
+    // - Второй элемент - 2
     let img_3_2_tl = gsap.timeline({ repeat: -1 });
     img_3_2_tl.add(gsap.to(img_3_2, { y: -20, duration: 10, ease: "linear" }))
     img_3_2_tl.add(gsap.to(img_3_2, { y: 0, duration: 10, ease: "linear" }))
 
-    // 3-3
+    // - Второй элемент - 3
     let img_3_3_tl = gsap.timeline({ repeat: -1 });
     img_3_3_tl.add(gsap.to(img_3_3, { zIndex: 4, duration: 0, }));
     img_3_3_tl.add(gsap.to(img_3_3, { x: -230, duration: 6, ease: "linear", }));
     img_3_3_tl.add(gsap.to(img_3_3, { zIndex: 1, duration: 0, }));
     img_3_3_tl.add(gsap.to(img_3_3, { x: 0, duration: 6, ease: "linear" }));
 
-    // 4
+    // - Третий элемент
     let img_4 = document.getElementById('serv-img-4');
     let img_4_tl = gsap.timeline({ repeat: -1 });
     img_4_tl.add(gsap.to(img_4, { rotate: 360, duration: 30, ease: "none" }))
--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,27 +1,69 @@
 import Swiper from 'swiper/bundle';
 
-// var init = false;
+// --- Слайдер в блоке features на главной странице
+var index_features_init = false;
 
-// function archiveSlider() {
-//   if (window.innerWidth <= 1279) {
-//     if (!init) {
-//       init = true;
-//       var archiveSlider = new Swiper(".press-body__slider", {
-//         direction: "horizontal",
-//         slidesPerView: "auto",
-//         spaceBetween: 31,
-//         centeredSlides: true,
+function index_features_slider() {
+  if (window.innerWidth <= 1279) {
+    if (!index_features_init) {
+      index_features_init = true;
+      var index_features_slider = new Swiper(".features__slider", {
+        direction: "horizontal",
+        spaceBetween: 10,
 
-//         pagination: {
-//           el: ".press-body__slider-pagination",
-//           clickable: true,
-//         },
-//       });
-//     }
-//   } else if (init) {
-//     archiveSlider.destroy();
-//     init = false;
-//   }
-// }
-// archiveSlider();
-// window.addEventListener("resize", archiveSlider);
+        breakpoints: {
+          320: {
+            slidesPerView: 1
+          },
+          767: {
+            slidesPerView: "auto"
+          }
+        },
+
+        pagination: {
+          el: ".features__slider-pagination",
+          clickable: true,
+        },
+      });
+    }
+  } else if (index_features_init) {
+    index_features_slider.destroy();
+    index_features_init = false;
+  }
+}
+index_features_slider();
+window.addEventListener("resize", index_features_slider);
+
+// --- Слайдер в блоке about на главной странице
+var index_about_init = false;
+
+function index_about_slider() {
+  if (window.innerWidth <= 1279) {
+    if (!index_about_init) {
+      index_about_init = true;
+      var index_about_slider = new Swiper(".about__slider", {
+        direction: "horizontal",
+        spaceBetween: 10,
+
+        pagination: {
+          el: ".about__slider-pagination",
+          clickable: true,
+        },
+
+        breakpoints: {
+          320: {
+            slidesPerView: 1
+          },
+          767: {
+            slidesPerView: "auto"
+          }
+        },
+      });
+    }
+  } else if (index_about_init) {
+    index_about_slider.destroy();
+    index_about_init = false;
+  }
+}
+index_about_slider();
+window.addEventListener("resize", index_about_slider);
